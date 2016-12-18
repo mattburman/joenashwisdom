05237c30611bd5b45ed0317d38bb844c1b706096
Matt Burman <matt@mattburman.com>
2016-12-18 12:47:31 +0000

    Update persistent storage

--- api.rb
+++ api.rb
@@ -7,17 +7,27 @@ set :port, 8183
 class Nash < Sinatra::Base
   set :port, 8183
   model = Ebooks::Model.load("model/jna_sh.model")
+
+  def initialize
+    super()
+    @FILE = "wisdom_number.txt"
+  end
+
   get '/' do
-    @wisdomNumber = 0
     length = 140
     @wisdom = model.make_statement(length)
+    wisdom_number = File.read(@FILE).to_i+1
+    File.open(@FILE, 'w') { |file| file.write(wisdom_number) }
+    @wisdomNumber = wisdom_number
     erb :index
   end
 
   get '/api' do
     content_type :json
+    wisdom_number = File.read(@FILE).to_i+1
+    File.open(@FILE, 'w') { |file| file.write(wisdom_number) }
     length = 140
-    { :wisdom => model.make_statement(length), :wisdomNumber => 0 }.to_json
+    { :wisdom => model.make_statement(length), :wisdomNumber => wisdom_number }.to_json
   end
 end
 
