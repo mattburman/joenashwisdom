fb997e3450921f184939e2a2f57c531cae6b1af2
Matt Burman <matt@mattburman.com>
2016-12-18 10:16:10 +0000

    Add port

--- api.rb
+++ api.rb
@@ -1,8 +1,8 @@
 require 'twitter_ebooks'
 require 'sinatra'
+:set port, 8183
 
 model = Ebooks::Model.load("model/jna_sh.model")
-lol = model.make_statement(140)
 
 get '/' do
   model.make_statement(140)
