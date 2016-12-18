a55a6c8b9123b4083f6dcadce83f45f54e7c68f7
Matt Burman <matt@mattburman.com>
2016-12-18 12:11:47 +0000

    ERB and api

--- api.rb
+++ api.rb
@@ -1,10 +1,25 @@
 require 'twitter_ebooks'
 require 'sinatra'
+require 'json'
+
 set :port, 8183
 
-model = Ebooks::Model.load("model/jna_sh.model")
+class Nash < Sinatra::Base
+  set :port, 8183
+  model = Ebooks::Model.load("model/jna_sh.model")
+  get '/' do
+    @wisdomNumber = 0
+    length = 140
+    @wisdom = model.make_statement(length)
+    erb :index
+  end
 
-get '/api' do
-  model.make_statement(140)
+  get '/api' do
+    content_type :json
+    length = 140
+    { :wisdom => model.make_statement(length), :wisdomNumber => 0 }.to_json
+  end
 end
+
+Nash.run!
 
